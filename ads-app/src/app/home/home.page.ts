75a8272a5980431b64972454c4e3f94f30237060
nabil_khmissi <bilakhmissi@gmail.com>
2023-09-30 22:44:29 +0100

    add search functionnality

--- ads-app/src/app/home/home.page.ts
+++ ads-app/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AdsService } from './services/ads.service';
 import { AuthService } from '../shared/services/auth.service';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +10,17 @@ import { AuthService } from '../shared/services/auth.service';
 })
 export class HomePage implements OnInit {
 
+  empty = false;
+
   constructor(private _ads: AdsService, private _auth: AuthService) {
     _auth.getUserFromLS()
   }
 
-  ads$ = this._ads.getAllAds();
+  ads$ = this._ads.getAllAds().pipe(
+    tap(res => {
+      this.empty = res.length === 0 ? true : false
+    })
+  );
   authUser$ = this._auth.authenticatedUser$;
   ngOnInit() {
 
