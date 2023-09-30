474c5544fdce6cf4aa2cd437cfe4dbddbaa08ef4
nabil_khmissi <bilakhmissi@gmail.com>
2023-09-30 22:31:55 +0100

    add login required

--- ads-app/src/app/login/login.page.ts
+++ ads-app/src/app/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../shared/services/auth.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map, tap } from 'rxjs';
+import { AlertService } from '../shared/services/alert.service';
 
 @Component({
   selector: 'app-login',
@@ -9,11 +11,21 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private _auth: AuthService, private _router: Router) { }
+  constructor(private _auth: AuthService, private _router: Router, private _activatedRoute: ActivatedRoute, private _alert: AlertService) { }
 
 
   email: string = "";
   password: string = "";
+
+  message = this._activatedRoute.queryParams.pipe(
+    map(route => route['message']),
+    tap(message => {
+      if (message) {
+        this._alert.showNotification(message)
+      }
+    })
+  ).subscribe()
+
 
   ngOnInit() {
   }
